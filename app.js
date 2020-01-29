9090af1e4291bdde2e7735520e2af5f5c583dace
Nicholas Bratton <47389141+nick-bratton@users.noreply.github.com>
2020-01-29 14:01:36 +0100

    moves cron interval to .env

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const util = require('util');
 const readFile = util.promisify(fs.readFile);
 const readdir = util.promisify(fs.readdir);
 
-let cronInterval = '15 10 * * MON';
 let logFileExtLength = 3; // i.e., 3 for .log; 2 for .md
 
 let transporterConfig = {
@@ -78,6 +77,6 @@ main().catch((err) => {
 	throw err;
 })
 
-// new Cron(cronInterval, function() {
+// new Cron(process.env.CRON_INTERVAL, function() {
 // 	main();
 // }, null, true, 'Europe/Berlin');
